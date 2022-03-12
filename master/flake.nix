8ffd0bb109b5a52a1fcc1359bc7542da8707c1a7
hugosenari <hugosenari@gmail.com>
2022-03-12 04:25:58 -0300

    chore: re index nimcdl

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Circuit Design language made in Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimcdl-master.flake = false;
+  inputs.src-nimcdl-master.owner = "endes123321";
+  inputs.src-nimcdl-master.ref   = "refs/heads/master";
+  inputs.src-nimcdl-master.repo  = "nimcdl";
+  inputs.src-nimcdl-master.type  = "gitlab";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimcdl-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimcdl-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
